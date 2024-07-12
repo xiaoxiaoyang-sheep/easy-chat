e246a8a91f01325095537861947a2562e80939f7
赵晨炀 <904852749@qq.com>
2024-07-12 17:42:16 +0800

    feat: api响应输出处理

---
+++ pkg/resultx/httpResponse.go
@@ -0,0 +1,67 @@
+/**
+ * @author: Yanko/xiaoxiaoyang-sheep
+ * @doc:
+ **/
+
+package resultx
+
+import (
+	"context"
+	"easy-chat/pkg/xerr"
+	"github.com/pkg/errors"
+	"github.com/zeromicro/go-zero/core/logx"
+	zerr "github.com/zeromicro/x/errors"
+	"google.golang.org/grpc/status"
+	"net/http"
+)
+
+type Response struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func Success(data interface{}) *Response {
+	return &Response{
+		Code: 200,
+		Msg:  "",
+		Data: data,
+	}
+}
+
+func Fail(code int, err string) *Response {
+	return &Response{
+		Code: code,
+		Msg:  err,
+		Data: nil,
+	}
+}
+
+func OkHandler(_ context.Context, v interface{}) any {
+	return Success(v)
+}
+
+func ErrHandler(name string) func(ctx context.Context, err error) (int, any) {
+	return func(ctx context.Context, err error) (int, any) {
+		errcode := xerr.SERVER_COMMON_ERROR
+		errmsg := xerr.ErrMsg(errcode)
+
+		causeErr := errors.Cause(err)
+		var e *zerr.CodeMsg
+		if errors.As(causeErr, &e) {
+			errcode = e.Code
+			errmsg = e.Msg
+		} else {
+			if gsstatus, ok := status.FromError(causeErr); ok {
+				grpcCode := int(gsstatus.Code())
+				errcode = grpcCode
+				errmsg = gsstatus.Message()
+			}
+		}
+
+		// 日志记录
+		logx.WithContext(ctx).Errorf("【%s】err %v", name, err)
+
+		return http.StatusBadRequest, Fail(errcode, errmsg)
+	}
+}
